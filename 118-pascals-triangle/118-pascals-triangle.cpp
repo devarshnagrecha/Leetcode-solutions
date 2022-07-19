27e21959f795ccd473683d91003aade1d268b5a3
Devarsh Nagrecha <75675815+devarshnagrecha@users.noreply.github.com>
2022-07-19 08:24:55 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (90.76%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
    vector<vector<int>> generate(int numRows) 
    {
+       // answer 2d vector
 	    vector<vector<int>> ret;
+       
 	    for (int i = 0; i < numRows; i++) 
         {
+            // next row will have size, one more than previous row
 		    vector<int> row(i + 1, 1);
 		    for (int j = 1; j < i; j++) 
             {
+                // current colomn in current row will be sum of current column in previous
+                // row and previous column in previous row
 			    row[j] = ret[i - 1][j] + ret[i - 1][j - 1];
 		    }
 		    ret.push_back(row);
