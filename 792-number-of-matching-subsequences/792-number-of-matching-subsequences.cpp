3f0f8d261c24742af51f48b255d8246bf2500e76
Devarsh Nagrecha <75675815+devarshnagrecha@users.noreply.github.com>
2022-07-20 16:22:44 +0530

    Time: 222 ms (84.49%), Space: 48.1 MB (62.91%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
-    int numMatchingSubseq(string S, vector<string>& words) 
+     // General Idea:
+    // - Brute force checking will time out, we need efficent way to look up words
+    // - Create an vector that stores indices for each character a-z in S
+    // - Then for each word, do a binary search for next index for current character in word 
+    //   that is greater than the index we last found for the alst character
+    // - If it doesn't exist, word doesn't exist, otherwise continue to search for word
+    
+	int numMatchingSubseq (string S, vector<string>& words) 
     {
-        vector<const char*> waiting[128];
+		vector<vector<int>> alpha (26);
+		for (int i = 0; i < S.size (); ++i) 
+            alpha[S[i] - 'a'].push_back (i);
         
-        for (auto &w : words)
-            waiting[w[0]].push_back(w.c_str());
-        
-        for (char c : S) 
+		int res = 0;
+
+		for (const auto& word : words) 
         {
-            auto advance = waiting[c];
-            waiting[c].clear();
-            
-            for (auto it : advance)
-                waiting[*++it].push_back(it);
-        }
-        
-        return waiting[0].size();
-    }
+			int x = -1;
+			bool found = true;
+
+			for (char c : word) 
+            {
+				auto it = upper_bound (alpha[c - 'a'].begin (), alpha[c - 'a'].end (), x);
+                
+				if (it == alpha[c - 'a'].end ())
+                    found = false;
+                
+				else 
+                    x = *it;
+			}
+
+			if (found) res++;
+		}
+
+		return res;
+	}
 };
