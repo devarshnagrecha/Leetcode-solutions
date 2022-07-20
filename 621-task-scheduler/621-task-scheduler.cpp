41014c4a4c8c82ee78b29d0bfe18a63690cc24ef
Devarsh Nagrecha <75675815+devarshnagrecha@users.noreply.github.com>
2022-07-20 17:09:42 +0530

    Time: 159 ms (35.95%), Space: 34.4 MB (92.98%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    
+    /*First count the number of occurrences of each element.
+    Let the max frequency seen be M for element E
+    We can schedule the first M-1 occurrences of E, each E will be followed by at least N CPU cycles in between successive schedules of E
+    Total CPU cycles after scheduling M-1 occurrences of E = (M-1) * (N + 1) // 1 comes for the CPU cycle for E itself
+    Now schedule the final round of tasks. We will need at least 1 CPU cycle of the last occurrence of E. If there are multiple tasks with frequency M, they will all need 1 more cycle.
+    Run through the frequency dictionary and for every element which has frequency == M, add 1 cycle to result.
+    If we have more number of tasks than the max slots we need as computed above we will return the length of the tasks array as we need at least those many CPU cycles.*/
     int leastInterval(vector<char>& tasks, int n) 
     {
         unordered_map<char,int>mp;
