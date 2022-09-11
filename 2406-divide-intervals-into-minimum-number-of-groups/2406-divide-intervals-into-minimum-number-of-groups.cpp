00ad1b1e3131209b23ffead8de61120ab42e7549
Devarsh Nagrecha <75675815+devarshnagrecha@users.noreply.github.com>
2022-09-11 10:37:58 +0530

    Time: 1130 ms (25.00%), Space: 118.1 MB (25.00%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     
-    int minGroups(vector<vector<int>>& ints) 
-    {
-        sort(begin(ints), end(ints));
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for (const auto &i : ints) 
-        {
-            if (!pq.empty() && pq.top() < i[0])
-                pq.pop();
-            pq.push(i[1]);
-        }
-        return pq.size();
+     int minGroups(vector<vector<int>>& intervals) 
+     {
+        map<int, int> A;
+        for (auto& v : intervals)
+            A[v[0]]++, A[v[1] + 1]--;
+         
+        int res = 0, cur = 0;
+         
+        for (auto& v : A)
+            res = max(res, cur += v.second);
+        return res;
     }
 };
