0e22e742e9e1e1df3f3e6501b5734a891f585c9b
Devarsh Nagrecha <75675815+devarshnagrecha@users.noreply.github.com>
2022-11-03 00:44:49 +0530

    Time: 18 ms (82.16%), Space: 15.8 MB (71.78%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,25 +2,54 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) 
     {
-        sort (nums.begin(), nums.end());
-        
-        vector <int> ans;
-        
-        for (int i=0; i<nums.size();)
+        int count1 = 0, count2 = 0, candidate1 = -1, candidate2 = -1;
+
+        for (int num : nums)
         {
-            int x = nums[i], count = 1;
-            i++;
-            while (i<nums.size() && nums[i]==x)
+            if (num == candidate1)
+                count1++;
+
+            else if (num == candidate2)
+                count2++;
+
+            else if (count1 == 0)
             {
-                count++;
-                i++;
+                count1++;
+                candidate1 = num;
             }
-                
-            
-            if (count>nums.size()/3)
-                ans.push_back(x);
+
+            else if (count2 == 0)
+            {
+                count2++;
+                candidate2 = num;
+            }
+
+            else
+            {
+                count1--;
+                count2--;
+            }
         }
-        
+
+        int temp1 = 0, temp2 = 0;
+
+        // verify if candidate is majority
+        for (int num : nums)
+        {
+            if (num == candidate1)
+                temp1++;
+            if (num == candidate2)
+                temp2++;
+        }
+
+        vector<int> ans;
+
+        if (temp1 > nums.size() / 3)
+            ans.push_back(candidate1);
+
+        if (temp2 > nums.size() / 3 && candidate1 != candidate2)
+            ans.push_back(candidate2);
+
         return ans;
     }
 };
