6a5b20d97b23ed4f91809a3f42d75063a3384b64
Devarsh Nagrecha <75675815+devarshnagrecha@users.noreply.github.com>
2023-06-07 14:02:41 +0530

    Added solution - LeetHub

---
+++ Minimal Cost - GFG/minimal-cost.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minimizeCost(vector<int>& height, int n, int k) 
+    {
+        vector<int>dp(n);
+        dp[0]=0;
+        for (int i=1; i<n; i++)
+        {
+            int x = INT_MAX, j = i-1;
+            while(j>=0 && i-j<=k)
+            {
+                x = min (x, dp[j]+abs(height[i]-height[j]));
+                j--;
+            }
+            dp[i]=x;
+        }
+        return dp[n-1];
+        // Code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> N >> K;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.minimizeCost(arr, N, K) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
